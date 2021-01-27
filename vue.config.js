ebe424fe897c546962d75e52ae279d494d94181b
Bila Arta <bilaarta@gmail.com>
2021-01-27 19:27:56 +0700

    set deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Project-Web-Desa-Client-Side/'
+    : '/'
 }
